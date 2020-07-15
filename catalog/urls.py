0bf6a5abf560f5be70122d5f0461ac870035034b
meshvm <meshvm@gmail.com>
2020-07-16 02:07:12 +0530

    catalog app added

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
+from django.conf.urls import include
 from django.urls import path
 from . import views
-
+#Add URL maps to redirect the base URL to our application
 urlpatterns = [
-
+    path('', views.library_list, name='library_list'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
+    'blog',
     'catalog',
 ]
 
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'  
 
 USE_I18N = True
 
